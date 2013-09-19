d94cdb1d87a55457d2a445470c59137a8bd06c1e
Anders Engström <anders.engstrom@gtech.com>
2013-09-19 08:11:21 +0200

    Initial commit

---
+++ app.rb
@@ -0,0 +1,28 @@
+#!/usr/bin/env ruby
+require 'sinatra'
+
+before do
+    @start_timer = Time.now
+end
+after do
+    puts "Time elapsed: #{Time.now - @start_timer}"
+end
+
+get '/foo/*' do |title|
+    erb :index, :locals => {:title => title}
+end
+
+class Foo
+    def each
+        10.times {|i| yield "#{i}"}
+    end
+end
+
+get '/bar/*' do |path|
+    "Hello, you're asking for #{path}?"
+    # Return value can be a string
+    # An array [status (Fixnum), headers (Hash), body (#each)]
+    # An array [status, body]
+    # #each (yielding string)
+    # Statuscode
+end
