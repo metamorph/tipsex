a9f345a55cb9f2c1ef688c14c41aacf67e59d777
Anders Engström <anders.engstrom@gtech.com>
2013-09-20 00:00:40 +0200

    Add "in-memory" implementation and TinyMCE controller

--- app.rb
+++ app.rb
@@ -1,29 +1,56 @@
 #!/usr/bin/env ruby
 require 'sinatra'
 
-before do
-    @start_timer = Time.now
-end
-after do
-    puts "Time elapsed: #{Time.now - @start_timer}"
-end
+class Post
+    # Static field holding all posts
+    @posts = []
 
-get '/foo/*' do |title|
-    title = "Default" if title.empty?
-    erb :index, :locals => {:title => title}
-end
+    def self.create(sender, subject, body)
+        @posts << Post.new(@posts.size, sender, subject, body)
+    end
+    def self.all
+        @posts
+    end
+    def self.find(id)
+        @posts.find do |elm|
+            id == elm.id
+        end
+    end
+    def self.init
+        puts "INITIALIZING POSTS"
+        create("Klas", "Hello", "First post here!")
+        create("Anders", "Zip zap", "Foo falkjalk sdhgf akdjf aldjk h ")
+        create("Martin", "Lorem fck", "Lorem ipsum dolor sit amet, consectetur
+        adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore
+        magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+        ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+        irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+        fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+        sunt in culpa qui officia deserunt mollit anim id est laborum.")
+    end
 
-class Foo
-    def each
-        10.times {|i| yield "#{i}"}
+    attr_reader :id, :sender, :subject, :body, :created_at
+    def initialize(id, sender, subject, body)
+        @id, @sender, @subject, @body, @created_at =
+            id, sender, subject, body, Time.now
     end
 end
 
-get '/bar/*' do |path|
-    "Hello, you're asking for #{path}?"
-    # Return value can be a string
-    # An array [status (Fixnum), headers (Hash), body (#each)]
-    # An array [status, body]
-    # #each (yielding string)
-    # Statuscode
+configure do
+    Post.init 
 end
+
+get '/' do
+    redirect to("/posts")
+end
+get '/posts' do 
+    erb :posts, :locals => {:title => "Inlägg", :posts => Post.all.reverse, :page => :posts}
+end
+get '/new_post' do
+    erb :new_post, :locals => {:title => "Nytt inlägg", :page => :write}
+end
+post '/new_post' do
+    Post.create(request["from"], request["subject"], request["body"])
+    redirect to("/posts")
+end
+
